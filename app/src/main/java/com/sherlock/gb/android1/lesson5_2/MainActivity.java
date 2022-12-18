b9c3cfe2207ee790c34d8185ad917aea6620d4ee
Sherlock <sherlockh989@gmail.com>
2022-12-18 09:34:49 +0200

    Initial commit

---
+++ app/src/main/java/com/sherlock/gb/android1/lesson5_2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.sherlock.gb.android1.lesson5_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText site = findViewById(R.id.editText);
+        Button go = findViewById(R.id.button);
+        go.setOnClickListener(v -> {
+            String url = site.getText().toString();
+            Uri uri = Uri.parse(url);
+            Intent browser = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(browser);
+        });
+    }
+}
